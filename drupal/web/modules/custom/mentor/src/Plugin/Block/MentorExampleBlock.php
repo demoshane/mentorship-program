210f2419de5179a3bbbbdd3f91d297b1e70f3f98
Tuomas Leppänen <tuomas.leppanen@wunder.io>
2017-09-15 20:32:59 +0300

    #Converts example block to configurable one. - Adds field for both mentor and mentee. - Uses information in markup of the block.

--- drupal/web/modules/custom/mentor/src/Plugin/Block/MentorExampleBlock.php
+++ drupal/web/modules/custom/mentor/src/Plugin/Block/MentorExampleBlock.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\mentor\Plugin\Block;
 
+use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Block\BlockBase;
 
 /**
@@ -16,9 +17,46 @@ class MentorExampleBlock extends BlockBase {
   /**
    * {@inheritdoc}
    */
+  public function blockForm($form, FormStateInterface $form_state) {
+    $form['mentor'] = array(
+      '#type' => 'textfield',
+      '#title' => $this->t('Mentor'),
+      '#description' => $this->t('Please input your name'),
+      '#default_value' => !empty($this->configuration['mentor']) ? $this->configuration['mentor'] : '',
+      '#maxlength' => 64,
+      '#size' => 64,
+      '#weight' => '1',
+    );
+    $form['mentee'] = array(
+      '#type' => 'textfield',
+      '#title' => $this->t('Mentee'),
+      '#description' => $this->t("Please input your mentee's name"),
+      '#default_value' => !empty($this->configuration['mentee']) ? $this->configuration['mentee'] : 'no one',
+      '#maxlength' => 64,
+      '#size' => 64,
+      '#weight' => '2',
+    );
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockSubmit($form, FormStateInterface $form_state) {
+    $this->configuration['mentee'] = $form_state->getValue('mentee');
+    $this->configuration['mentor'] = $form_state->getValue('mentor');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
   public function build() {
+    // Better way for this?
+    $mentee = !empty($this->configuration['mentee']) ? $this->configuration['mentee'] : 'no one';
+    $mentor = !empty($this->configuration['mentor']) ? $this->configuration['mentor'] : '';
     return array(
-      '#markup' => t("Hello Mr. Raimonds."),
+      '#markup' => t("Hello @mentor. You're mentor of @mentee. Welcome to the mentorship program both of you!", array('@mentor' => $mentor, '@mentee' => $mentee)),
     );
   }
 }
