0ef0b8058208228217ee9909b7480c11464fda43
Tuomas Leppänen <tuomas.leppanen@wunder.io>
2017-09-15 19:22:29 +0300

    #Wundertools customizations. D8 basic install.
    Description of changes:
    Contains basics for the training.

--- drupal/web/sites/default/settings.php
+++ drupal/web/sites/default/settings.php
@@ -83,3 +83,4 @@ $settings['container_yamls'][] = __DIR__ . '/services.yml';
 if (file_exists(__DIR__ . '/settings.local.php')) {
    include __DIR__ . '/settings.local.php';
 }
+$settings['install_profile'] = 'standard';
